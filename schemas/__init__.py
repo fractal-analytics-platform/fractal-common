db02f2115fbbc106ccd360f6ed0061b89e9f1184
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-01-31 10:30:34 +0100

    Refactor `ApplyWorkflow*` schemas into applyworkflow

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,9 +1,4 @@
-from datetime import datetime
-from typing import List
-from typing import Optional
-
-from sqlmodel import SQLModel
-
+from .applyworkflow import *  # noqa: F403
 from .manifest import *  # noqa: F403
 from .project import *  # noqa: F403
 from .state import *  # noqa: F403
@@ -12,53 +7,10 @@ from .workflow import *  # noqa: F403
 
 
 __all__ = (
-    (
-        "ApplyWorkflowBase",
-        "ApplyWorkflowCreate",
-        "ApplyWorkflowRead",
-    )
-    + project.__all__  # noqa: F405
+    +project.__all__  # noqa: F405
     + task.__all__  # noqa: F405
     + workflow.__all__  # noqa: F405
+    + applyworkflow.__all__  # noqa: F405
     + manifest.__all__  # noqa: F405
     + state.__all__  # noqa: F405
 )
-
-
-class ApplyWorkflowBase(SQLModel):
-    """
-    Base class for ApplyWorkflow
-
-    Attributes:
-        input_dataset_id: TBD
-        output_dataset_id: TBD
-        workflow_id: TBD
-        overwrite_input: TBD
-        worker_init: TBD
-        working_dir: TBD
-    """
-
-    project_id: int
-    input_dataset_id: int
-    output_dataset_id: Optional[int]
-    workflow_id: Optional[int]
-    overwrite_input: bool = False
-    worker_init: Optional[str]
-    working_dir: Optional[str]
-
-
-class ApplyWorkflowCreate(ApplyWorkflowBase):
-    pass
-
-
-class ApplyWorkflowRead(ApplyWorkflowBase):
-    id: int
-    start_timestamp: datetime
-    status: str
-    log: Optional[str]
-    history: Optional[List[str]]
-
-    def sanitised_dict(self):
-        d = self.dict()
-        d["start_timestamp"] = str(self.start_timestamp)
-        return d
---
+++ schemas/applyworkflow.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from typing import List
+from typing import Optional
+
+from sqlmodel import SQLModel
+
+
+__all__ = (
+    "ApplyWorkflowBase",
+    "ApplyWorkflowCreate",
+    "ApplyWorkflowRead",
+)
+
+
+class ApplyWorkflowBase(SQLModel):
+    """
+    Base class for ApplyWorkflow
+
+    Attributes:
+        input_dataset_id: TBD
+        output_dataset_id: TBD
+        workflow_id: TBD
+        overwrite_input: TBD
+        worker_init: TBD
+        working_dir: TBD
+    """
+
+    project_id: int
+    input_dataset_id: int
+    output_dataset_id: Optional[int]
+    workflow_id: Optional[int]
+    overwrite_input: bool = False
+    worker_init: Optional[str]
+    working_dir: Optional[str]
+
+
+class ApplyWorkflowCreate(ApplyWorkflowBase):
+    pass
+
+
+class ApplyWorkflowRead(ApplyWorkflowBase):
+    id: int
+    start_timestamp: datetime
+    status: str
+    log: Optional[str]
+    history: Optional[List[str]]
+
+    def sanitised_dict(self):
+        d = self.dict()
+        d["start_timestamp"] = str(self.start_timestamp)
+        return d
