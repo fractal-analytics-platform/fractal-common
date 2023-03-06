ceda8e01ae99eab5eafc00b911551a2ad8aa6754
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-03-06 14:28:54 +0100

    Add more tests

--- tests/test_project.py
+++ tests/test_project.py
@@ -4,7 +4,25 @@ from pydantic.error_wrappers import ValidationError
 
 from schemas import DatasetCreate
 from schemas import DatasetRead
+from schemas import ProjectCreate
 from schemas import ResourceRead
+
+
+def test_project_create():
+    # Successful creation
+    p = ProjectCreate(name="my project", project_dir="/somewhere")
+    debug(p)
+    # Check that whitespaces are stripped from beginning/end of string
+    NAME = "some project name"
+    p = ProjectCreate(name=f"  {NAME}  ", project_dir="/somewhere")
+    debug(p)
+    assert p.name == NAME
+    # Fail due to empty strings
+    with pytest.raises(ValidationError):
+        ProjectCreate(name="  ", project_dir="/somewhere")
+
+    with pytest.raises(ValidationError):
+        ProjectCreate(name="my project", project_dir=" ")
 
 
 def test_dataset_create():
