d13af684cf9cbc31d43be213bb4f9b1169c311b6
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-03-06 11:49:57 +0100

    Re-activate test

--- tests/test_project.py
+++ tests/test_project.py
@@ -22,12 +22,10 @@ def test_dataset_create():
         d = DatasetCreate(name="  ")
 
 
-@pytest.mark.xfail(reason="In-progress")
 def test_dataset_read():
     d = DatasetRead(id=1, project_id=1, resource_list=[], name="n")
     debug(d)
     r = ResourceRead(id=1, dataset_id=1, path="/something")
     debug(r)
-    # FIXME: The following statement fails
     d2 = DatasetRead(id=1, project_id=1, resource_list=[r], name="n")
     debug(d2)
