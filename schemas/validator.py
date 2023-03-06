5d15f8ff371021c7f1395fc71e4f3cbe8221dccf
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-03-06 14:08:44 +0100

    Improve docstrings and error messages

--- schemas/validator.py
+++ schemas/validator.py
@@ -1,18 +1,28 @@
 def valstr(attribute: str):
+    """
+    Check that a string attribute is not an empty string.
+    """
+
     def val(string: str):
         s = string.strip()
         if not s:
-            raise ValueError(f"'{attribute}' cannot be empty")
+            raise ValueError(f"String attribute '{attribute}' cannot be empty")
         return s
 
     return val
 
 
 def valint(attribute: str):
+    """
+    Check that an integer attribute (meant to be the ID of a database entry) is
+    greater or equal to 1.
+    """
+
     def val(integer: int):
         if integer < 1:
             raise ValueError(
-                f"'{attribute}' cannot be less than 1 (given {integer})"
+                f"Integer attribute '{attribute}' cannot be less than 1 "
+                f"(given {integer})"
             )
         return integer
 
