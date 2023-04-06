353986cfe338f74da1eb35bf21ac7d8c5e0c3d56
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-04-06 09:38:42 +0200

    Add cache_dir attribute to User schemas (close #36)

--- tests/test_user.py
+++ tests/test_user.py
@@ -17,3 +17,17 @@ def test_user_create():
     # With invalid slurm_user attribute
     with pytest.raises(ValidationError):
         u = UserCreate(email="a@b.c", password="asd", slurm_user="  ")
+    # With valid cache_dir
+    CACHE_DIR = "/xxx"
+    u = UserCreate(email="a@b.c", password="asd", cache_dir=f"{CACHE_DIR}   ")
+    assert u.cache_dir == CACHE_DIR
+    # With invalid cache_dir attribute
+    with pytest.raises(ValidationError) as e:
+        u = UserCreate(email="a@b.c", password="asd", cache_dir="  ")
+    debug(e.value)
+    assert "cannot be empty" in e.value.errors()[0]["msg"]
+    # With invalid cache_dir attribute
+    with pytest.raises(ValidationError) as e:
+        u = UserCreate(email="a@b.c", password="asd", cache_dir="xxx")
+    debug(e.value)
+    assert "must be an absolute path" in e.value.errors()[0]["msg"]
