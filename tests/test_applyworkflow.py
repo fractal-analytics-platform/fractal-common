8946b429a9004d903dd68674028355ea0b805444
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-05-08 13:23:19 +0200

    Remove assertion from test

--- tests/test_applyworkflow.py
+++ tests/test_applyworkflow.py
@@ -4,11 +4,10 @@ from schemas import ApplyWorkflowCreate
 
 
 def test_apply_workflow_create():
-    # Valid ApplyWorkflowCreate instance / without output_dataset_id
+    # Valid ApplyWorkflowCreate instance
     valid_args = dict(
         overwrite_input=True,
         worker_init="WORKER INIT",
     )
     job = ApplyWorkflowCreate(**valid_args)
     debug(job)
-    assert job
