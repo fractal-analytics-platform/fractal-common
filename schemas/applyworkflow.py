9fa567cfa75d7e33fbd666c34f9861bea843d772
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-07-17 11:30:53 +0200

    Rename `{start,end}_task` to `{first,last}_task_index`, add validators to `ApplyWorkflowCreate`, and add a test (ref #81)

--- schemas/applyworkflow.py
+++ schemas/applyworkflow.py
@@ -26,13 +26,41 @@ class _ApplyWorkflowBase(BaseModel):
 
 class ApplyWorkflowCreate(_ApplyWorkflowBase):
 
-    start_task: Optional[int] = None
-    end_task: Optional[int] = None
+    first_task_index: Optional[int] = None
+    last_task_index: Optional[int] = None
 
     # Validators
     _worker_init = validator("worker_init", allow_reuse=True)(
         valstr("worker_init")
     )
+
+    @validator("first_task_index", always=True)
+    def first_task_index_non_negative(cls, v, values):
+        if v is not None and v < 0:
+            raise ValueError(
+                    f"first_task_index cannot be negative (given: {v})"
+                    )
+        return v
+
+    @validator("last_task_index", always=True)
+    def first_last_task_indices(cls, v, values):
+        """
+        Check `last_task_index` is not smaller than `first_task_index`.
+        """
+        if v is not None and v < 0:
+            raise ValueError(
+                    f"last_task_index cannot be negative (given: {v})"
+                    )
+
+        first_task_index = values.get("first_task_index")
+        last_task_index = v
+        if first_task_index is not None and last_task_index is not None:
+            if first_task_index > last_task_index:
+                raise ValueError(
+                        f"{first_task_index=} cannot be larger than "
+                        f"{last_task_index=}"
+                        )
+        return v
 
 
 class ApplyWorkflowRead(_ApplyWorkflowBase):
@@ -48,6 +76,8 @@ class ApplyWorkflowRead(_ApplyWorkflowBase):
     history: Optional[list[str]]
     working_dir: Optional[str]
     working_dir_user: Optional[str]
+    first_task_index: Optional[int]
+    last_task_index: Optional[int]
 
     def sanitised_dict(self):
         d = self.dict()
--- tests/test_applyworkflow.py
+++ tests/test_applyworkflow.py
@@ -1,13 +1,23 @@
 from devtools import debug
+import pytest
 
 from schemas import ApplyWorkflowCreate
 
 
 def test_apply_workflow_create():
     # Valid ApplyWorkflowCreate instance
-    valid_args = dict(
-        overwrite_input=True,
-        worker_init="WORKER INIT",
-    )
+    valid_args = dict(worker_init="WORKER INIT")
     job = ApplyWorkflowCreate(**valid_args)
     debug(job)
+
+    with pytest.raises(ValueError) as e:
+        job = ApplyWorkflowCreate(first_task_index=-1)
+    debug(e)
+
+    with pytest.raises(ValueError) as e:
+        job = ApplyWorkflowCreate(last_task_index=-1)
+    debug(e)
+
+    with pytest.raises(ValueError) as e:
+        job = ApplyWorkflowCreate(first_task_index=2, last_task_index=0)
+    debug(e)
