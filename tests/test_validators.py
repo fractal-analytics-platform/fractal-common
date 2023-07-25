e9caf641bf48340c6318c8c5d7f306fd176410d7
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-07-25 12:47:32 +0200

    Integrate `test_common.py` from fractal-server (close #88)

---
+++ tests/test_validators.py
@@ -0,0 +1,29 @@
+import pytest
+
+from schemas import ProjectCreate
+from schemas import ResourceCreate
+from schemas import WorkflowTaskCreate
+
+
+def test_fail_valstr():
+    ProjectCreate(name="  valid    name ")
+    with pytest.raises(ValueError):
+        ProjectCreate(name=None)
+    with pytest.raises(ValueError):
+        ProjectCreate(name="   ")
+
+
+def test_fail_val_absolute_path():
+    ResourceCreate(path="/valid/path")
+    with pytest.raises(ValueError):
+        ResourceCreate(path=None)
+    with pytest.raises(ValueError):
+        ResourceCreate(path="./invalid/path")
+
+
+def test_fail_valint():
+    WorkflowTaskCreate(order=1)
+    with pytest.raises(ValueError):
+        WorkflowTaskCreate(order=None)
+    with pytest.raises(ValueError):
+        WorkflowTaskCreate(order=-1)
